baa73a71244b573a0f8761938ed17202976e434a
Ugo PELISSIER <ugo.pelissier@mines-paristech.fr>
2025-03-21 16:21:11 +0100

    hotfix: remove undefined arguments

--- predict.sh
+++ predict.sh
@@ -1,7 +1,4 @@
 python -m graphphysics.predict \
             --predict_parameters_path=training_config/coarse-aneurysm.json \
-            --batch_size=1 \
-            --num_workers=0 \
-            --prefetch_factor=0 \
             --model_path=checkpoints/predict_test.ckpt \
             --no_edge_feature
