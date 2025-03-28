850701d22dea844909253f5c5ccb4019a81734ff
Paul Garnier <44346877+DonsetPG@users.noreply.github.com>
2025-03-28 13:26:05 +0100

    feat: add smooth L1 loss (#20)
    * feat: add smooth L1 loss
    
    * misc: fix docstring
    
    * misc: add default value for beta in smoothL1

--- graphphysics/utils/loss.py
+++ graphphysics/utils/loss.py
@@ -7,6 +7,24 @@ from graphphysics.utils.nodetype import NodeType
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
+def _prepare_mask_for_loss(
+    network_output: torch.Tensor,
+    node_type: torch.Tensor,
+    masks: list[NodeType],
+    selected_indexes: torch.Tensor = None,
+):
+    mask = node_type == masks[0]
+    for i in range(1, len(masks)):
+        mask = torch.logical_or(mask, node_type == masks[i])
+
+    if selected_indexes is not None:
+        n, _ = network_output.shape
+        nodes_mask = ~torch.isin(torch.arange(n), selected_indexes).to(device)
+        mask = torch.logical_and(nodes_mask, mask)
+
+    return mask
+
+
 class L2Loss(_Loss):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -40,16 +58,53 @@ class L2Loss(_Loss):
             This method calculates the L2 loss only for nodes of the types specified in 'masks'.
             If 'selected_indexes' is provided, those nodes are excluded from the loss calculation.
         """
-        mask = node_type == masks[0]
-        for i in range(1, len(masks)):
-            mask = torch.logical_or(mask, node_type == masks[i])
-
-        if selected_indexes is not None:
-            n, _ = network_output.shape
-            nodes_mask = ~torch.isin(torch.arange(n), selected_indexes).to(device)
-            mask = torch.logical_and(nodes_mask, mask)
-
+        mask = _prepare_mask_for_loss(
+            network_output, node_type, masks, selected_indexes
+        )
         errors = ((network_output - target) ** 2)[mask]
+        return torch.mean(errors)
+
+
+class L1SmoothLoss(_Loss):
+    def __init__(self, beta: float = 1.0, **kwargs):
+        super().__init__(**kwargs)
+        self.beta = beta
+
+    @property
+    def __name__(self):
+        return "L1Smooth"
+
+    def forward(
+        self,
+        target: torch.Tensor,
+        network_output: torch.Tensor,
+        node_type: torch.Tensor,
+        masks: list[NodeType],
+        selected_indexes: torch.Tensor = None,
+    ) -> torch.Tensor:
+        """
+        Computes L1Smooth loss for nodes of specific types.
+
+        Args:
+            target (torch.Tensor): The target values.
+            network_output (torch.Tensor): The predicted values from the network.
+            node_type (torch.Tensor): Tensor containing the type of each node.
+            masks (list[NodeType]): List of NodeTypes to include in the loss calculation.
+            selected_indexes (torch.Tensor, optional): Indexes of nodes to exclude from the loss calculation.
+
+        Returns:
+            torch.Tensor: The L1Smooth loss for the specified node types.
+
+        Note:
+            This method calculates the L1Smooth loss only for nodes of the types specified in 'masks'.
+            If 'selected_indexes' is provided, those nodes are excluded from the loss calculation.
+        """
+        mask = _prepare_mask_for_loss(
+            network_output, node_type, masks, selected_indexes
+        )
+        errors = F.smooth_l1_loss(
+            network_output, target, reduction="none", beta=self.beta
+        )[mask]
         return torch.mean(errors)
 
 
