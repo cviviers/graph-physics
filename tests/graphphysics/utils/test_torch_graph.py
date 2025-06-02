56d489e47e87a8e2a64336fe4657a4a0107edd4b
bazziamir24 <bazziamir@outlook.com>
2025-06-02 08:52:50 +0200

    lint polishing

--- tests/graphphysics/utils/test_torch_graph.py
+++ tests/graphphysics/utils/test_torch_graph.py
@@ -21,21 +21,21 @@ def test_meshdata_to_graph():
     assert graph.x.shape[0] == 1923
     assert graph.pos.shape[0] == 1923
 
+
 def test_meshdata_to_graph_3d():
-    points = np.array([
-        [0.0, 0.0, 0.0],
-        [1.0, 0.0, 0.0],
-        [0.0, 1.0, 0.0],
-        [0.0, 0.0, 1.0],
-    ], dtype=np.float32)
+    points = np.array(
+        [
+            [0.0, 0.0, 0.0],
+            [1.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [0.0, 0.0, 1.0],
+        ],
+        dtype=np.float32,
+    )
 
-    cells = np.array([
-        [0, 1, 2, 3]
-    ], dtype=np.int32)
+    cells = np.array([[0, 1, 2, 3]], dtype=np.int32)
 
-    point_data = {
-        'dummy_feature': np.array([1.0, 2.0, 3.0, 4.0], dtype=np.float32)
-    }
+    point_data = {"dummy_feature": np.array([1.0, 2.0, 3.0, 4.0], dtype=np.float32)}
 
     graph = meshdata_to_graph(
         points=points,
@@ -45,11 +45,13 @@ def test_meshdata_to_graph_3d():
 
     assert graph.x.shape[0] == 4
 
+
 def test_mesh_to_graph():
     mesh = get_meshs_from_vtu()[0]
     graph = mesh_to_graph(mesh=mesh)
     assert graph.x.shape[0] == 1923
     assert graph.pos.shape[0] == 1923
+
 
 def test_khop_edges():
     mesh = get_meshs_from_vtu()[0]
